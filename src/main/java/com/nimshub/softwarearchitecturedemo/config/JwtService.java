4f829fa27a3d2a1bef6156fae5b807303c822085
madushan <ranasinhemadushan@gmail.com>
2023-08-12 14:04:21 +0530

    application config and jwt service

---
+++ src/main/java/com/nimshub/softwarearchitecturedemo/config/JwtService.java
@@ -0,0 +1,15 @@
+package com.nimshub.softwarearchitecturedemo.config;
+
+import com.nimshub.softwarearchitecturedemo.user.User;
+
+public class JwtService {
+
+
+    /**
+     * generate token using user information
+     */
+    public String  generateToken(User user) {
+        // todo implement genearate token method
+        return null;
+    }
+}
