c7626b848e550748182600f8c12ff3336d310588
madushan <ranasinhemadushan@gmail.com>
2023-08-12 21:38:40 +0530

    refactor

--- src/main/java/com/nimshub/softwarearchitecturedemo/user/UserService.java
+++ src/main/java/com/nimshub/softwarearchitecturedemo/user/UserService.java
@@ -1,7 +1,6 @@
 package com.nimshub.softwarearchitecturedemo.user;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.SecondaryRow;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
