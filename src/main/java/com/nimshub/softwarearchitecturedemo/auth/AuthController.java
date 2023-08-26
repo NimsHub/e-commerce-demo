a83b6eb41d5ae34c6d080329be0387388c3d8909
madushan <ranasinhemadushan@gmail.com>
2023-08-27 00:02:34 +0530

    email verification

--- src/main/java/com/nimshub/softwarearchitecturedemo/auth/AuthController.java
+++ src/main/java/com/nimshub/softwarearchitecturedemo/auth/AuthController.java
@@ -2,10 +2,13 @@ package com.nimshub.softwarearchitecturedemo.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -14,8 +17,8 @@ public class AuthController {
     private final AuthService service;
 
     @PostMapping("/register")
-    public ResponseEntity<LoginResponse> register(
-            @RequestBody RegistrationRequest request
+    public ResponseEntity<RegistrationResponse> register(
+            @Valid @RequestBody  RegistrationRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
