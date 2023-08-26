fd510523f86565859ce3683e6961a7061ae061c2
madushan <ranasinhemadushan@gmail.com>
2023-08-26 23:55:41 +0530

    applicaiton error handler

---
+++ src/main/java/com/nimshub/softwarearchitecturedemo/exceptions/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.nimshub.softwarearchitecturedemo.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@Builder
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private HttpStatus status;
+    private String message;
+    private String uri;
+    private ZonedDateTime timeStamp;
+    private StackTraceElement trace;
+}
+
